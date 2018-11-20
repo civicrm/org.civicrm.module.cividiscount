1c5ba03a973f4799d62108e6d8e62d2348368a8c
Jitendra Purohit <jitendra@fuzion.co.nz>
2018-11-20 18:46:36 +0530

    Apply autodiscount if multiple code satisfies the auto discount condition

--- cividiscount.php
+++ cividiscount.php
@@ -435,7 +435,7 @@ function cividiscount_civicrm_buildAmount($pageType, &$form, &$amounts) {
           }
         }
       }
-      if ($autodiscount) {
+      if ($autodiscount && $discountApplied) {
         break;
       }
     }
