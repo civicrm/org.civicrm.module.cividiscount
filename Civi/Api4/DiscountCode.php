cac24be04ab4fb0c9bd73ef390415570a83cab21
Coleman Watts <coleman@civicrm.org>
2020-08-26 12:20:24 -0400

    Add APIv4

---
+++ Civi/Api4/DiscountCode.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Api4;
+
+/**
+ * DiscountCode entity.
+ *
+ * Provided by the CiviDiscount extension.
+ *
+ * @see https://docs.civicrm.org/discount/en/latest/
+ * @package Civi\Api4
+ */
+class DiscountCode extends Generic\DAOEntity {
+
+}
