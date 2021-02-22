441a901b9a1572bfd25c71a9adb7628484308289
eileen <emcnaughton@wikimedia.org>
2021-02-22 15:22:11 +1300

    Fix save to stop overwriting calculated value

--- cividiscount.php
+++ cividiscount.php
@@ -593,14 +593,6 @@ function cividiscount_civicrm_membershipTypeValues(&$form, &$membershipTypeValue
       list($value, $label) = _cividiscount_calc_discount($values['minimum_fee'], $values['name'], $discount, $discountCalculator->isAutoDiscount());
       $values['minimum_fee'] = $value;
       $values['name'] = $label;
-
-      // set total amount to be same as the calculated discount
-      // this will overwrite the submitted total amount
-      if (!empty($form->_submitValues['membership_type_id'])) {
-        if ($values['member_of_contact_id'] == $form->_submitValues['membership_type_id'][0] && $values['id'] == $form->_submitValues['membership_type_id'][1]) {
-          $form->_submitValues['total_amount'] = $value;
-        }
-      }
     }
   }
 
